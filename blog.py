0122537d924e54a1c6c6d8dccf7f39594bc861b1
Aaron Wilkinson <aaronwilkinson@gmail.com>
2018-04-07 18:16:58 +0000

    build structure of website and backend processing

--- blog.py
+++ blog.py
@@ -4,6 +4,7 @@ import tornado.web
 import tornado.log
 import tornado.web
 import queries
+from markdown2 import markdown
 
 from jinja2 import \
     Environment, PackageLoader, select_autoescape
@@ -24,16 +25,63 @@ class TemplateHandler(tornado.web.RequestHandler):
     
 class MainHandler(TemplateHandler):
     def get (self):
-        self.render_template("base.html", {})
+        self.render_template("home.html", {})
+
+class CommentHandler(TemplateHandler):
+    def post(self, post_id):
+        comment = self.get_body_argument('comment')
+        self.session.query('''
+            INSERT INTO comment VALUES (
+            DEFAULT,
+            %(comment)s,
+            %(post_id)s,
+            current_timestamp)
+            ''', {'comment': comment, 'post_id': post_id})
+        slug = self.session.query('SELECT slug FROM post WHERE id = %(id)s', {'id': post_id})[0]['slug']
+        self.redirect("/post/" + slug)
 
 class PostHandler(TemplateHandler):
     def get(self, slug):
-        pass
+        blog_post_data = self.session.query('''
+            SELECT * 
+            FROM post 
+            INNER JOIN author ON author.id = post.author_id
+            WHERE slug = %(slug)s
+            ''', {'slug': slug})
+        blog_post_data = blog_post_data[0]
+        blog_post_data['body'] = markdown(blog_post_data['body'])
+        comments = self.session.query('''
+            SELECT c.comment, c.comment_post_datetime
+            FROM comment c
+            INNER JOIN post p on p.id = c.post_id
+            WHERE p.slug = %(slug)s
+            ''', {'slug': slug})
+        self.render_template("post.html", {'post': blog_post_data, 'comments': comments})
+
+class AuthorsHandler(TemplateHandler):
+    def get(self):
+        authors = self.session.query('SELECT * FROM author')
+        self.render_template("authors.html", {'authors': authors})
+
+class AuthorPostsHandler(TemplateHandler):
+    def get(self, author_id):
+        author = self.session.query('SELECT name FROM author WHERE id = %(id)s', {'id': author_id})[0]
+        posts = self.session.query('''
+            SELECT id, title, slug, post_date
+            FROM post
+            WHERE author_id = %(id)s
+            ORDER BY post_date
+            ''', {'id': author_id})
+        self.render_template("author_posts.html", {'posts': posts, 'author': author})
+        
 
 def make_app():
     return tornado.web.Application([
         (r"/", MainHandler),
+        (r"/post/(.*)/comment", CommentHandler),
         (r"/post/(.*)", PostHandler),
+        (r"/authors", AuthorsHandler),
+        (r"/author/(.*)", AuthorPostsHandler),
         (r"/static/(.*)", 
             tornado.web.StaticFileHandler, {'path': 'static'}),
         ], autoreload=True)
@@ -41,5 +89,7 @@ def make_app():
 if __name__ == "__main__":
     tornado.log.enable_pretty_logging()
     app = make_app()
-    app.listen(int(os.environ.get('PORT', '8080')))
+    # setting a static port to allow for database access at same time
+    app.listen('8888')
+    # app.listen(int(os.environ.get('PORT', '8000')))
     tornado.ioloop.IOLoop.current().start()
